ba94544016cba8997bc31bff6715af63e2cb464f
bombang <ehsancros20182018@gmail.com>
2017-03-24 00:07:07 +0430

    tele_bom_bang

--- anticrash.sh
+++ anticrash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true ; do
-  for entr in permag.sh ; do
+  for entr in tele_bom_bang.sh ; do
     entry="${entr/.sh/}"
     tmux kill-session -t $entry
     rm -rf ~/.telegram-cli/$entry/data/animation/*
